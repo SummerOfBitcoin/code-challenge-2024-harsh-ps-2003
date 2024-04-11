6291e10ce3cd7e1ebf03facf8a4a3a56a656955c
Harsh Pratap Singh <harshpratapsingh8210@gmail.com>
2024-04-11 18:09:14 +0530

    removing coinbase uncessary

--- main.py
+++ main.py
@@ -33,71 +33,72 @@ def read_transactions(mempool_dir: str) -> List[Dict]:
 
 def create_coinbase_transaction(miner_address: str, block_height: int, block_reward: int, transactions: List[Dict]) -> dict:
 
-    """
-    Create a coinbase transaction.
+    # """
+    # Create a coinbase transaction.
 
-    :param miner_address: The address of the miner to receive the block reward.
-    :param block_height: The height of the block being mined.
-    :param block_reward: The total block reward (block subsidy + transaction fees).
-    :param witness_commitment: The witness commitment hash.
-    :return: A dictionary representing the coinbase transaction.
-    """
-    # Coinbase transaction's input
-    vin = [{
-        'txid': '0000000000000000000000000000000000000000000000000000000000000000',
-        'vout': 0xffffffff,
-        'scriptSig': {
-            'asm': f'{block_height}',
-            'hex': block_height.to_bytes(4, 'little').hex() + '...'  # Placeholder for additional data to ensure scriptSig length is valid
-        },
-        'sequence': 0xffffffff,
-        'coinbase': '...'  # Placeholder for coinbase data (e.g., extra nonce and miner-defined data)
-    }]
+    # :param miner_address: The address of the miner to receive the block reward.
+    # :param block_height: The height of the block being mined.
+    # :param block_reward: The total block reward (block subsidy + transaction fees).
+    # :param witness_commitment: The witness commitment hash.
+    # :return: A dictionary representing the coinbase transaction.
+    # """
+    # # Coinbase transaction's input
+    # vin = [{
+    #     'txid': '0000000000000000000000000000000000000000000000000000000000000000',
+    #     'vout': 0xffffffff,
+    #     'scriptSig': {
+    #         'asm': f'{block_height}',
+    #         'hex': block_height.to_bytes(4, 'little').hex() + '...'  # Placeholder for additional data to ensure scriptSig length is valid
+    #     },
+    #     'sequence': 0xffffffff,
+    #     'coinbase': '...'  # Placeholder for coinbase data (e.g., extra nonce and miner-defined data)
+    # }]
 
-    # Output for the miner's block reward
-    vout_miner_reward = {
-        'value': block_reward,
-        'scriptPubKey': {
-            'asm': f'OP_DUP OP_HASH160 {miner_address} OP_EQUALVERIFY OP_CHECKSIG',
-            'hex': '...'  # Placeholder for the actual hex representation
-        }
-    }
+    # # Output for the miner's block reward
+    # vout_miner_reward = {
+    #     'value': block_reward,
+    #     'scriptPubKey': {
+    #         'asm': f'OP_DUP OP_HASH160 {miner_address} OP_EQUALVERIFY OP_CHECKSIG',
+    #         'hex': '...'  # Placeholder for the actual hex representation
+    #     }
+    # }
 
-    # Output for the witness commitment
-    vout_witness_commitment = {
-        'value': 0,
-    }
+    # # Output for the witness commitment
+    # vout_witness_commitment = {
+    #     'value': 0,
+    # }
 
-    # The coinbase transaction itself
-    coinbase_tx = {
-        'version': 4,
-        'vin': vin,
-        'vout': [vout_miner_reward, vout_witness_commitment],
-        'locktime': 0,
-        'witness': []  # Empty witness data as it's a coinbase transaction
-    }
+    # # The coinbase transaction itself
+    # coinbase_tx = {
+    #     'version': 4,
+    #     'vin': vin,
+    #     'vout': [vout_miner_reward, vout_witness_commitment],
+    #     'locktime': 0,
+    #     'witness': []  # Empty witness data as it's a coinbase transaction
+    # }
 
-    # result = get_raw_transaction("segwit", coinbase_tx)
-    # serilaized_coinbase_txn = result[1]
-    # coinbase_byte = hashlib.sha256(hashlib.sha256(serilaized_coinbase_txn).digest()).digest() 
-    # return coinbase_tx, coinbase_byte.hex()
-    wtxids = []
-    # coinbase wtxid
-    wtxids.append("0000000000000000000000000000000000000000000000000000000000000000")
-    # other txids
-    for txid in selected_txids:
-        tx_file = os.path.join("mempool", f"{txid}.json")
-        with open(tx_file, 'r') as file:
-            tx = json.load(file)
-            if all("witness" not in input for input in tx["vin"]):
-                wtxids.append(txid) # if legacy then wtxid = txid
-            try:
-                raw_tx = get_raw_transaction(tx)
-            except Exception as e:
-                selected_txids.remove(txid) 
-            wtxids.append((hashlib.sha256(hashlib.sha256(raw_tx).digest()).digest()).hex())
-    witnessroot = merkleroot(wtxids)
-    witnessComm = (hashlib.sha256(hashlib.sha256(witnessroot + WITNESS_RESERVED_VALUE).digest()).digest()).hex()
+    # # result = get_raw_transaction("segwit", coinbase_tx)
+    # # serilaized_coinbase_txn = result[1]
+    # # coinbase_byte = hashlib.sha256(hashlib.sha256(serilaized_coinbase_txn).digest()).digest() 
+    # # return coinbase_tx, coinbase_byte.hex()
+    # wtxids = []
+    # # coinbase wtxid
+    # wtxids.append("0000000000000000000000000000000000000000000000000000000000000000")
+    # # other txids
+    # for txid in selected_txids:
+    #     tx_file = os.path.join("mempool", f"{txid}.json")
+    #     with open(tx_file, 'r') as file:
+    #         tx = json.load(file)
+    #         if all("witness" not in input for input in tx["vin"]):
+    #             wtxids.append(txid) # if legacy then wtxid = txid
+    #         try:
+    #             raw_wtx = get_raw_transaction(tx)
+    #         except Exception as e:
+    #             selected_txids.remove(txid) 
+    #         wtxids.append((hashlib.sha256(hashlib.sha256(raw_wtx).digest()).digest()).hex())
+    # witnessroot = merkleroot(wtxids)
+    # concatenated_data = witnessroot + WITNESS_RESERVED_VALUE
+    # print(hashlib.sha256(hashlib.sha256(concatenated_data).digest()).digest()).hex()
     coinbase_tx_hex = "010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff2503233708184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100ffffffff02f595814a000000001976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9ed63ee4a27911ebd2f2288d993013260c3a2d7c36d2b82b5f85184eba2d8abdfb70120000000000000000000000000000000000000000000000000000000000000000000000000"
     return coinbase_tx_hex
 
