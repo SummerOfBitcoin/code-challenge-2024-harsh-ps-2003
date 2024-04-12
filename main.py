b9e6211de7a4911760b5e758e0bb1dc4dceab877
Harsh Pratap Singh <harshpratapsingh8210@gmail.com>
2024-04-12 20:48:29 +0530

    radha

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 
 DIFFICULTY_TARGET = 0x0000FFFF00000000000000000000000000000000000000000000000000000000
 MAX_BLOCK_SIZE = 1000000  # 1 MB
-WITNESS_RESERVED_VALUE = b"0000000000000000000000000000000000000000000000000000000000000000"
+WITNESS_RESERVED_VALUE = '0000000000000000000000000000000000000000000000000000000000000000'
 
 def read_transactions(mempool_dir: str) -> List[Dict]:
     data = {}
@@ -92,10 +92,13 @@ def create_coinbase_transaction(miner_address: str, block_height: int, block_rew
             # tx = json.load(file)
             # if all("witness" not in input for input in tx["vin"]):
                 # wtxids.append(txid[::-1]) # if legacy then wtxid = txid
+    with open('wtxids.txt', 'w') as file:
+        for wtxid in wtxids:
+            file.write(wtxid + '\n')
     witnessroot = merkleroot(wtxids)
-    concatenated_data = witnessroot.hex() + WITNESS_RESERVED_VALUE.hex()
-    # witnessComm = (hashlib.sha256(hashlib.sha256(bytes.fromhex(concatenated_data)).digest()).digest()).hex()
-    witnessComm = "f07e1888bbb9639596683aeddd84d8e1b34163ad4b960efff6a51f83e4cd7124"
+    concatenated_data = witnessroot.hex() + WITNESS_RESERVED_VALUE
+    witnessComm = (hashlib.sha256(hashlib.sha256(bytes.fromhex(concatenated_data)).digest()).digest()).hex()
+    # witnessComm = "f07e1888bbb9639596683aeddd84d8e1b34163ad4b960efff6a51f83e4cd7124"
     coinbase_tx_hex = f"010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff2503233708184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100ffffffff02f595814a000000001976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9ed{witnessComm}0120000000000000000000000000000000000000000000000000000000000000000000000000"
     # mid = hashlib.sha256(hashlib.sha256(bytes.fromhex(coinbase_tx_hex)).digest()).digest()  
     # global coinbase_txid
