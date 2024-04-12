1f88c807341b8d9041f527ce3ce470a9cd4875e3
Harsh Pratap Singh <harshpratapsingh8210@gmail.com>
2024-04-12 13:36:55 +0530

    coinbase txid

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def create_coinbase_transaction(miner_address: str, block_height: int, block_rew
     # witnessroot = merkleroot(wtxids)
     # concatenated_data = witnessroot.hex() + WITNESS_RESERVED_VALUE.hex()
     # witnessComm = (hashlib.sha256(hashlib.sha256(bytes.fromhex(concatenated_data)).digest()).digest()).hex()
-    coinbase_tx_hex = "010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff2503233708184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100ffffffff02f595814a000000001976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9edf07e1888bbb9639596683aeddd84d8e1b34163ad4b960efff6a51f83e4cd71240120000000000000000000000000000000000000000000000000000000000000000000000000"
+    coinbase_tx_hex = "010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff2503233708184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100ffffffff02f595814a000000001976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9edf3098fdeffeaa74a166ab497bb8ee34e90aeb8d4b73be520c4b42d537710e4e20120000000000000000000000000000000000000000000000000000000000000000000000000"
     return coinbase_tx_hex
 
 def construct_block(transactions: List[Dict], miner_address: str, block_height: int) -> Dict:
@@ -227,6 +227,9 @@ def output_to_file(transactions: List[Dict]):
         file.write(header_hex + "\n")
         # Write the coinbase transaction 
         file.write(coinbase_tx_hex + "\n")
+        mid = hashlib.sha256(hashlib.sha256(bytes.fromhex(coinbase_tx_hex)).digest()).digest()  
+        coinbase_txid = mid[::-1].hex()
+        file.write(coinbase_txid + "\n")
         # Write the txids of all transactions (excluding the coinbase transaction)
         for tx in selected_txids:
                 file.write(tx + "\n")
